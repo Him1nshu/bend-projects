78d37b72572a9f9a3d756e9e6ce9c194ea534533
Him1nshu <astng888@gmail.com>
2026-08-16 16:21:10 +0530

    feat(project):added crud operations

--- tasktracker/task-cli.js
+++ tasktracker/task-cli.js
@@ -1,17 +1,89 @@
-const fs=require('fs');
-const tasks=[];
-if(!fs.existsSync('tasks.json')){
-    fs.writeFileSync('tasks.json',JSON.stringify(tasks));
+const fs = require('fs');
+
+if (!fs.existsSync('tasks.json')) {
+    fs.writeFileSync('tasks.json', '[]');
 }
-else{
-    
-    const desc=process.argv[1];
-    const status=process.argv[2];
-    const date=new Date().toISOString();
-    const temp=[desc,status,date];
-    tasks.push(temp);
-    fs.writeFileSync('tasks.json',JSON.stringify(tasks));
-    const tasks=fs.readFileSync("tasks.json","utf-8");
-    const arr=JSON.parse(tasks);
-    console.log(tasks);
+
+const data = fs.readFileSync('tasks.json', 'utf8');
+const tasks = JSON.parse(data);
+
+const command = process.argv[2];
+
+if (command === 'add') {
+    const description = process.argv[3];
+
+    const task = {
+        id: tasks.length + 1,
+        description: description,
+        status: 'todo',
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString()
+    };
+
+    tasks.push(task);
+
+    fs.writeFileSync('tasks.json', JSON.stringify(tasks, null, 2));
+
+    console.log(`Task added successfully (ID: ${task.id})`);
+}
+else if (command == 'delete') {
+    const taskno = process.argv[3];
+
+    const index = tasks.findIndex(task => task.id == taskno);
+
+    if (index != -1) {
+        tasks.splice(index, 1);
+
+        fs.writeFileSync(
+            "tasks.json",
+            JSON.stringify(tasks, null, 2)
+        );
+
+        console.log("Task deleted successfully");
+    }
+    else {
+        console.log("Task not found");
+    }
+}
+else if (command == 'update') {
+    const taskno = process.argv[3];
+    const description = process.argv[4];
+
+    const index = tasks.findIndex(task => task.id == taskno);
+
+    if (index != -1) {
+        tasks[index].description = description;
+        tasks[index].updatedAt = new Date().toISOString();  
+        fs.writeFileSync(
+            "tasks.json",
+            JSON.stringify(tasks, null, 2)
+        );
+
+        console.log("Task updated successfully");
+    }
+    else {
+        console.log("Task not found");
+    }
+}
+else if (command == 'mark-in-progress' || command == 'mark-done') {
+    const taskno = process.argv[3];
+
+    const index = tasks.findIndex(task => task.id == taskno);
+
+    if (index != -1) {
+        tasks[index].status =
+            command == 'mark-done' ? 'done' : 'in-progress';
+
+        tasks[index].updatedAt = new Date().toISOString();
+
+        fs.writeFileSync(
+            "tasks.json",
+            JSON.stringify(tasks, null, 2)
+        );
+
+        console.log("Task status updated");
+    }
+    else {
+        console.log("Task not found");
+    }
 }
